f21b7121c82ddb7c5083e7ea075dc0be80b01971
robrichardsdpt <robrichardsdpt@gmail.com>
2020-09-22 16:45:21 -0400

    Updated config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://stormy-plains-65398.herokuapp.com/',
+  production: 'https://stormy-plains-65398.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
