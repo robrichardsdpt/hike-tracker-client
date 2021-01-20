71c333b75a4d9cf50a9543f28dca3c9ab2ca878a
Rob <robrichardsdpt@gmail.com>
2021-01-20 13:05:22 -0500

    separating keys so that each mountain is accounted for

--- assets/scripts/hike/ui.js
+++ assets/scripts/hike/ui.js
@@ -31,11 +31,21 @@ const onIndexSuccess = function (response) {
     return accumulator += hike.elevation
   }, 0)
   const mountainsClimbedArray = store.hikes.map(hike => {
-    return hike.mountainsClimbed
+     return hike.mountainsClimbed
   })
   console.log(mountainsClimbedArray)
   const mountainsTable = {}
   mountainsClimbedArray.forEach(mountain => mountainsTable[mountain] ? mountainsTable[mountain]++ : mountainsTable[mountain] = 1)
+  console.log(mountainsTable)
+  for(let key in mountainsTable) {
+    if (key.includes(',')) {
+      const splitKey = key.split(',')
+      for(let i = 0; i < splitKey.length; i++) {
+        mountainsTable[splitKey[i]] = mountainsTable[key]
+      }
+      delete mountainsTable[key] 
+    }
+  }
   console.log(mountainsTable)
   const hike48 = store.hikes.filter(hike => list.listOfNH.includes(hike.mountainsClimbed.toLowerCase()))
   console.log(hike48)
